834f8cdf1fb5efdba8620c58abf8a8eb051facb0
Nicholas Buckeridge <bucknich@gmail.com>
2020-10-26 18:45:45 +1000

    Working React + Wasm project 🎉

---
+++ example/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ExampleComponent } from 'gol-react'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
---
+++ example/webpack.config.js
@@ -0,0 +1,37 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx']
+  },
+};
